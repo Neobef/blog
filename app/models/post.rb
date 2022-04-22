7ba24b0ff3152857d52a45ba777a11e3d7539e76
Neobef <fabianmaldonadosaba@gmail.com>
2022-04-22 16:16:58 -0400

    se prueba correctamente la pagina

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :image_url, presence: true
+    validates :content, presence: true
+    before_save :censurar
+    
+    def censurar
+        # self.content = self.content.downcase.gsub("spoiler","")
+        self.content = self.content.gsub(/spoiler/i,"")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/create'
-  get 'post/index'
-  get 'post/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
